59fcf142559c98414386969cd291d3b4bb3f4322
n0rad <dev@norad.fr>
2011-07-19 15:32:11 +0200

    - reorganisation of client validation helper

--- js/src/test/java/net/awired/client/validation/tools/ClientValidationTestHelper.java
+++ js/src/test/java/net/awired/client/validation/tools/ClientValidationTestHelper.java
@@ -1,6 +1,8 @@
 package net.awired.client.validation.tools;
 
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import net.awired.client.bean.validation.js.domain.ClientConstraintViolation;
 import net.awired.client.bean.validation.js.service.ValidationService;
@@ -10,6 +12,7 @@ import org.mozilla.javascript.ContextFactory;
 import org.mozilla.javascript.NativeJavaObject;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.tools.shell.Global;
+import com.google.common.base.Preconditions;
 
 public class ClientValidationTestHelper {
 
@@ -42,22 +45,75 @@ public class ClientValidationTestHelper {
         }
     }
 
-    //TODO manage groups
-    public static <T> Set<ClientConstraintViolation> validateValue(ClientConstraintInfo info, Class<T> o,
+    public static <T> Set<ClientConstraintViolation> validateValue(ArrayList<ClientConstraintInfo> infos,
+            Class<T> oClass, String propertyName, Object value, Class<?>... groups) {
+        return rhinoValidate(infos, new Scenario<T>(oClass, propertyName, value), groups);
+    }
+
+    public static <T> Set<ClientConstraintViolation> validate(ArrayList<ClientConstraintInfo> infos, T obj,
             String propertyName, Object value, Class<?>... groups) {
-        ValidationService validationService = new ValidationService();
-        Object validationObject = validationService.getValidationObject(o, propertyName);
+        return rhinoValidate(infos, new Scenario<T>(obj), groups);
+    }
+
+    public static <T> Set<ClientConstraintViolation> validateValue(ClientConstraintInfo info, Class<T> oClass,
+            String propertyName, Object value, Class<?>... groups) {
+        ArrayList<ClientConstraintInfo> constraintInfos = new ArrayList<ClientConstraintInfo>();
+        constraintInfos.add(info);
+        return rhinoValidate(constraintInfos, new Scenario<T>(oClass, propertyName, value), groups);
+    }
+
+    public static <T> Set<ClientConstraintViolation> validate(ClientConstraintInfo info, T obj, String propertyName,
+            Object value, Class<?>... groups) {
+        ArrayList<ClientConstraintInfo> constraintInfos = new ArrayList<ClientConstraintInfo>();
+        constraintInfos.add(info);
+        return rhinoValidate(constraintInfos, new Scenario<T>(obj), groups);
+    }
+
+    public static <T> Set<ClientConstraintViolation> validateValue(Class<T> oClass, String propertyName,
+            Object value, Class<?>... groups) {
+        return rhinoValidate(null, new Scenario<T>(oClass, propertyName, value), groups);
+    }
+
+    public static <T> Set<ClientConstraintViolation> validate(T obj, Class<?>... groups) {
+        return rhinoValidate(null, new Scenario<T>(obj), groups);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    //TODO manage groups
+    private static <T> Set<ClientConstraintViolation> rhinoValidate(List<ClientConstraintInfo> info,
+            Scenario<T> scenario, Class<?>... groups) {
+        Preconditions.checkNotNull(info);
         ObjectMapper mapper = new ObjectMapper();
+        ValidationService validationService = new ValidationService();
         try {
-            String jsonContraints = mapper.writeValueAsString(validationObject);
-            String jsonValue = "{\"" + propertyName + "\":" + mapper.writeValueAsString(value) + "}";
-            cx.evaluateReader(scope, info.getJsConstraint(), "", 1, null);
             cx.evaluateString(scope, "validator = new Validator();", "", 1, null);
-            cx.evaluateString(scope,
-                    "validator.registerConstraint('" + info.getConstraintType() + "', " + info.getJsConstraintName()
-                            + ")", "", 1, null);
-            cx.evaluateString(scope, "cv = validator.validateValue(JSON.parse('" + jsonValue + "'), JSON.parse('"
-                    + jsonContraints + "'), '" + propertyName + "')", "validator", 1, null);
+
+            for (ClientConstraintInfo clientConstraint : info) {
+                cx.evaluateReader(scope, clientConstraint.getJsConstraint(), "", 1, null);
+                cx.evaluateString(scope, "validator.registerConstraint('" + clientConstraint.getConstraintType()
+                        + "', " + clientConstraint.getJsConstraintName() + ")", "", 1, null);
+            }
+
+            String query;
+            if (scenario.scenario == ScenarioEnum.PROPERTY) {
+                Object validationObject = validationService.getValidationObject(scenario.objClass,
+                        scenario.propertyName);
+                String jsonContraints = mapper.writeValueAsString(validationObject);
+                String jsonValue = "{\"" + scenario.propertyName + "\":" + mapper.writeValueAsString(scenario.value)
+                        + "}";
+                query = "cv = validator.validateValue(JSON.parse('" + jsonValue + "'), JSON.parse('" + jsonContraints
+                        + "'), '" + scenario.propertyName + "')";
+            } else if (scenario.scenario == ScenarioEnum.OBJECT) {
+                Object validationObject = validationService.getValidationObject(scenario.obj.getClass());
+                String jsonContraints = mapper.writeValueAsString(validationObject);
+                String jsonBean = mapper.writeValueAsString(scenario.obj);
+                query = "cv = new Validator().validate(JSON.parse('" + jsonBean + "'), JSON.parse('" + jsonContraints
+                        + "'))";
+            } else {
+                throw new RuntimeException("unknow scenario type");
+            }
+            cx.evaluateString(scope, query, "validator", 1, null);
             NativeJavaObject result = (NativeJavaObject) cx.evaluateString(scope, "retreaveConstrainViolations(cv);",
                     "retreave", 1, null);
             @SuppressWarnings("unchecked")
@@ -68,25 +124,34 @@ public class ClientValidationTestHelper {
         }
     }
 
-    //    List<ClientConstraintInfo> info,
+    ///////////////////////////////////////////////////////////////////////////////
 
-    //TODO manage groups
-    public static <T> Set<ClientConstraintViolation> validate(T o, Class<?>... groups) {
-        ValidationService validationService = new ValidationService();
-        Object validationObject = validationService.getValidationObject(o.getClass());
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String jsonContraints = mapper.writeValueAsString(validationObject);
-            String jsonBean = mapper.writeValueAsString(o);
-            cx.evaluateString(scope, "cv = new Validator().validate(JSON.parse('" + jsonBean + "'), JSON.parse('"
-                    + jsonContraints + "'))", "validator", 1, null);
-            NativeJavaObject result = (NativeJavaObject) cx.evaluateString(scope, "retreaveConstrainViolations(cv);",
-                    "retreave", 1, null);
-            @SuppressWarnings("unchecked")
-            Set<ClientConstraintViolation> constraintsViolations = (Set<ClientConstraintViolation>) result.unwrap();
-            return constraintsViolations;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    enum ScenarioEnum {
+        OBJECT, PROPERTY;
+    }
+
+    static class Scenario<T> {
+
+        private T obj;
+
+        private Class<T> objClass;
+        private String propertyName;
+        private Object value;
+        private ScenarioEnum scenario;
+
+        public Scenario(T o) {
+            Preconditions.checkNotNull(o);
+            this.obj = o;
+            this.scenario = ScenarioEnum.OBJECT;
+        }
+
+        public Scenario(Class<T> oClass, String propertyName, Object value) {
+            Preconditions.checkNotNull(oClass);
+            Preconditions.checkNotNull(propertyName);
+            this.objClass = oClass;
+            this.propertyName = propertyName;
+            this.value = value;
+            this.scenario = ScenarioEnum.PROPERTY;
         }
     }
 
